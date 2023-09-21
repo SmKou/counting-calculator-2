0f94c617e5a3602e5421bd73e4a848adc1041870
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-20 23:50:20 -0700

    Add counter slice (untested)

--- src/components/Counter/counterSlice.js
+++ src/components/Counter/counterSlice.js
@@ -5,12 +5,45 @@ export const counterSlice = createSlice({
     initialState: {
         value: 0,
         previous: [],
-        increment: 1
+        step: 1
     },
     reducers: {
-        setCount: (state, action) => ({ ...state, value: action.payload }),
-        setIncrement: (state, action) => ({ ...state, increment: action.payload }),
-        stackPrevious: (state, action) => ({ ...state, previous: [...state.previous, action.payload] }),
-        unstackPrevious: (state, action) => ({ ...state, previous: state.previous.slice(0, -1) })
+        setCount: (state, action) => ({...state, value: action.payload}),
+        setStep: (state, action) => ({...state, step: action.payload}),
+        stackPrevious: (state, action) => ({...state, previous: [...state.previous, action.payload]}),
+        unstackPrevious: (state, action) => ({...state, previous: state.previous.slice(0, -1) }),
+        incrementValue: (state) => ({...state, value: state.value + state.step}),
+        decrementValue: (state) => {
+            if (state.value - state.step < 0) {
+                return {...state, value: 0}
+            }
+            else {
+                return {...state, value: state.value - state.step}
+            }
+        },
+        incrementStep: (state) => ({...state, step: state.step + 1}),
+        decrementStep: (state) => {
+            if (state.step - 1 < 0) {
+                return {...state, step: 0}
+            }
+            else {
+                return {...state, step: state.step - 1}
+            }
+        }
     }
 })
+
+export const selectCount = state => state.counter.value
+export const selectPrevious = state => state.counter.previous
+export const selectStep = state => state.counter.step
+export const {
+    setCount,
+    setStep,
+    stackPrevious,
+    unstackPrevious,
+    incrementValue,
+    decrementValue,
+    incrementStep,
+    decrementStep,
+} = counterSlice.actions
+export default counterSlice.reducer
