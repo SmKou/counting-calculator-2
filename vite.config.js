cebad75f3da4506ffab5ec18e81246aea1693f47
SmKou <66178856+SmKou@users.noreply.github.com>
2023-10-04 14:46:08 -0700

    Add deployment by npm package gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 443
-  }
+    base: "/counting-calculator-2",
+    plugins: [react()],
+    server: {
+        host: true,
+        strictPort: true,
+        port: 443
+    }
 })
